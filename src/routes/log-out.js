397f2f0e13c03b488789268af9e5d8ea40623334
Hex <hexmusictheory@protonmail.com>
2023-10-02 15:01:29 +0100

    complete fourth challenge

--- src/routes/log-out.js
+++ src/routes/log-out.js
@@ -1,11 +1,14 @@
+const { removeSession } = require("../model/session")
+
 function post(req, res) {
-  /**
-   * [1] Get the session ID from the cookie
-   * [2] Remove that session from the DB
-   * [3] Remove the session cookie
-   * [4] Redirect back home
-   */
-  res.status(500).send("");
+  // [1] Get the session ID from the cookie
+  const sid = req.signedCookies.sid;
+  // [2] Remove that session from the DB
+  removeSession(sid);
+  // [3] Remove the session cookie
+  res.clearCookie("sid");
+  // [4] Redirect back home
+  res.redirect("/");
 }
 
 module.exports = { post };
